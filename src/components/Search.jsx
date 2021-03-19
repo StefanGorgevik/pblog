1b4648a46c8c69b770b9e83bec3a0b18de932ebc
Stefan Gorgevik <stefangorgevik@hotmail.com>
2021-03-19 22:19:54 +0100

    added search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import InputBase from '@material-ui/core/InputBase';
-import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 
 export default function Search() {
-
   return (
-    <Grid component="form" >
-      <IconButton aria-label="menu">
-        <MenuIcon />
-      </IconButton>
-      <InputBase
-        className=''
+    <Grid className="search-div">
+      <TextField
+        className='search'
         placeholder="Search articles"
         inputProps={{ 'aria-label': 'search-articles' }}
       />
       <IconButton type="submit"  aria-label="search">
         <SearchIcon />
       </IconButton>
-      <Divider className='' orientation="vertical" />
     </Grid>
   );
 }
